270b5fd4f61f4ffb4eb20a0e313788f97e83dc50
Gitlab CI <ruben.malchow@gmail.com>
2021-02-12 06:17:08 +0100

    caching test

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -4,5 +4,7 @@ export TAG=harbor.rand0m.me/public/${CI_PROJECT_NAME}:latest
 docker login -u ${CI_EMAIL} -p ${CI_PASSWORD} harbor.rand0m.me
 docker build -t ${TAG} -f scripts/Dockerfile .
 docker push ${TAG}
+
+docker login -u ${DH_EMAIL} -p ${DH_PW}
 docker tag ${TAG} rmalchow/${CI_PROJECT_NAME}:latest
 docker push rmalchow/${CI_PROJECT_NAME}:latest
