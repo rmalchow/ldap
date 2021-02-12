ce957c2b14032c11697fb409173fb46a7558f0c9
Gitlab CI <ruben.malchow@gmail.com>
2021-02-12 06:29:21 +0100

    caching test

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -6,6 +6,6 @@ docker login -u ${CI_EMAIL} -p ${CI_PASSWORD} harbor.rand0m.me
 docker build -t ${TAG} -f scripts/Dockerfile .
 docker push ${TAG}
 
-docker login -u ${DH_EMAIL} -p ${DH_PW} hub.docker.com
+docker login -u ${DH_USER} -p ${DH_PW} hub.docker.com
 docker tag ${TAG} rmalchow/${CI_PROJECT_NAME}:latest
 docker push rmalchow/${CI_PROJECT_NAME}:latest
