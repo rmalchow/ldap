2987949664d9cbf5a57f674e683a57bafd58ed93
Gitlab CI <ruben.malchow@gmail.com>
2021-07-08 09:05:39 +0200

    add aarch64

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 set +x
-export TAG=harbor.rand0m.me/public/${CI_PROJECT_NAME}:latest
+
+export TAG=harbor.rand0m.me/public/${CI_PROJECT_NAME}:latest-$(arch)
 docker login -u ${CI_EMAIL} -p ${CI_PASSWORD} harbor.rand0m.me
 docker build -t ${TAG} -f scripts/Dockerfile .
 docker push ${TAG}
